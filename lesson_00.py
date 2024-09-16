586c838cf5e40649a6a00542d2cced91447e6fc9
Ashley King <ashleymking567@gmail.com>
2024-09-16 10:48:07 -0400

    9/16 benchmark

---
+++ lesson_00.py
@@ -0,0 +1,34 @@
+print("Ashley")
+# Step 1 - Print your name in console
+
+fav_Show = "Avatar: The Last Airbender"
+# Step 2 - Create a variable (in snake case) to store your favorite show
+
+float_Point = 2.5
+birth_Month = 5
+# Step 3 - Using snake case, make variables with a float and integer 
+
+print(float_Point * birth_Month)
+# Step 4 - Print the product of the previous 2 variables
+
+fav_foods = ['macaroni & cheese', 'potato salad', 'cheesescake']
+# Step 5 - Create a variable that stores your favorite foods (atleast 3)
+
+age = 17
+if age >= 18:
+    print("You can get a driver's license")
+else: 
+    print("You cannot get a driver's license")
+# Step 6 - Create an if statement that tells someone if they can drive
+
+name = input("What is your name?: ")
+print(name)
+#Step 7 - Ask the user for their name and print it into the console
+
+for i in fav_foods:
+    print(i)
+# Step 8 - Create a for loop that prints each of your favorite foods
+
+while fav_foods:
+    break
+# Step 9 - Create a while loop that will never run
