a17bedd251f3b18f6aa9aae01ee41eee4d2078ce
Ashley King <ashleymking567@gmail.com>
2024-09-19 09:16:50 -0400

    9/17 assignment completion

--- 02_formatting.py
+++ 02_formatting.py
@@ -7,14 +7,14 @@ The password should not be case sensitive
 
 password = "bluebell"
 pass_attempt = input("Whats the Password?: ").lower()
-# Asks user for password and converts input into lower case
+# Asks user for password and modifies the input to be all lower case
 
 if pass_attempt == password:
     print("Correct!")
     # Creates condition for the input matching the password and prints "Correct!" if the attempt and password matches.
 else:
     print("Incorrect!")
-    # If input does not match the password, "Incorrect!" is printed
+    # Creates else statement that prints "Incorrect!"if the user's input and the password do not match.
 
 
 """
@@ -24,10 +24,11 @@ If the string is empty, print "invalid" otherwise print "valid"
 """
 
 word = input("Write something: ").strip()
+# Asks the user to write something and strips any extra space at the start and end of the string. 
 
 if word == "":
     print("Invalid!")
-    # Creates if statment for the "word" string being empty, the user will be told the input is invalid
+    # Creates an if statement for when the "word" string is empty (just having spaces also counts as empty) the program will print "Invalid!"
 else: 
     print("Valid!")
     # Creates else statment so if the "word" string is not empty, the program will print "Valid!"
@@ -42,9 +43,8 @@ It should replace all occurances regardless of captilization
 mad_lib = input("Write something about pets: ").lower()
 # Asks the user for input about pets and changes answer into lowercase
 new_mad_lib = mad_lib.replace("cat", "dog")
-# Replaces all instances of the word "cat" with "dog"
+# Replaces all instances of the word "cat" with "dog" in the user's input
 print(new_mad_lib)
-
 
 """
 TASK 4:
@@ -54,10 +54,12 @@ Write a program that takes a person's name and age as input and prints it
 
 name_input = input("What is your name? ")
 # Asks user for their name
-age_input = str(input("What is your age? "))
+age_input = input("What is your age? ")
 # Asks user for their age
 info = "Your name is {0} and you are {1}!".format(name_input, age_input)
+# Formats the string by replacing the placeholders with the variables in the format function
 print(info)
+
 """
 TASK 5:
 
@@ -66,14 +68,15 @@ The result should be rounded to the nearest tenth (1 decimal place)
 """
 
 decim_one = float(input("Write a decimal number: "))
+# Asks the user for a decimal number and converts the input into a float
 decim_two = float(input("Write another decimal number: "))
-# Ask the user for a decimal numbers
+# Asks the user for a second decimal number and converts the input into a float
 
 operation_quo = decim_one / decim_two
-# Divides the numbers the user inputted
+# Divides the two decimal numbers the user inputted
 
 result = f"The result of the division is {operation_quo:.1f}"
-# Formats the results of the division into a sentence and rounds the quotient to the tenth.
+# Formats the results of the division into the sentence by filling the placeholder with the quotient and rounds the quotient to the tenth (one decimal place).
 
 print(result)
 
